04b40843948cddbaba0db6560dbd08858a1e83f2
ninahffbs <nina.hffs@gmail.com>
2025-08-20 19:29:24 -0300

    adding correct data

--- src/repositories/clubs-repository.ts
+++ src/repositories/clubs-repository.ts
@@ -1,5 +1,5 @@
 import { ClubModel } from "../models/clubs-model";
-
+import fs from "fs/promises";
 const database = [
     {
         id: 1,
@@ -7,5 +7,7 @@ const database = [
     }
 ]
 export const findAllClubs = async(): Promise<ClubModel[]> => {
-    return database;
+    const data = await fs.readFile("./src/data/clubs.json", "utf-8");
+    const clubs: ClubModel[] = JSON.parse(data)
+    return clubs;
 }
