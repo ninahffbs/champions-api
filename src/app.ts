b17a153c66f8f44819bddd19fb501ae27a492c09
ninahffbs <nina.hffs@gmail.com>
2025-08-20 19:35:08 -0300

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { json, Request, Response } from "express";
 import { getPlayer } from "./controllers/players-controller";
 import router from "./routes";
+import cors from "cors";
 
 function createApp(){
     const app = express();
     app.use(json());
+    app.use(cors());
 
     app.use("/api", router);
     return app;
