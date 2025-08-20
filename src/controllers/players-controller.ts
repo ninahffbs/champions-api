235045b4b954455f9a2763d2063730ff9e30ae86
ninahffbs <nina.hffs@gmail.com>
2025-08-20 17:53:17 -0300

    post requests

--- src/controllers/players-controller.ts
+++ src/controllers/players-controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { getPlayerByIdService, getPlayerService } from "../services/players-service";
-import { ok } from "../utils/http-helper";
+import { createPlayerService, getPlayerByIdService, getPlayerService } from "../services/players-service";
+import { noContent, ok } from "../utils/http-helper";
 
 export const getPlayer = async(req:Request, res:Response) => {
     const httpResponse = await getPlayerService();
@@ -11,4 +11,17 @@ export const getPlayerById = async(req:Request, res:Response) => {
     const id = parseInt(req.params.id) //pega o id colocada na url e salva na variavel
     const httpResponse = await getPlayerByIdService(id);
     res.status(httpResponse.statusCode).json(httpResponse.body);
+}
+
+export const postPlayer = async(req: Request, resp: Response) => {
+    const bodyValue = req.body;
+    console.log(bodyValue);
+    const httpResponse = await createPlayerService(bodyValue);
+    if(httpResponse){
+        resp.status(httpResponse.statusCode).json(httpResponse?.body)
+    }
+    // else{
+    //     const response = await noContent();
+    //     resp.status(response.statusCode).json(response.body);
+    // }
 }
--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,5 +1,7 @@
-import { findAllPlayers, findPlayerById } from "../repositories/players-repository";
-import { noContent, ok } from "../utils/http-helper";
+import { PlayerModel } from "../models/player-models";
+import { findAllPlayers, findPlayerById, insertPlayer } from "../repositories/players-repository";
+import { badRequest, created, noContent, ok } from "../utils/http-helper";
+import { httpResponse } from "../models/http-response-models"
 
 export const getPlayerService = async()=>{
     const data = await findAllPlayers();
@@ -24,6 +26,20 @@ export const getPlayerByIdService = async(id: number) => {
     }
     else{
         response = noContent();
+    }
+    return response;
+}
+
+export const createPlayerService = async(player: PlayerModel) => {
+    let response = null;
+
+    if(Object.keys(player).length !== 0){
+        await insertPlayer(player);
+        response = created();
+        console.log(player);
+    }
+    else{
+        response = badRequest();
     }
     return response;
 }
